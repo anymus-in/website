9ba2ae559929eafa3cef3e8b3dd3ec42e03d1cbb
Mohit Hingorani <Mohithingorani2003@gmail.com>
2026-06-18 00:47:52 +0530

    swap page sections: add ProblemsSection, remove LogoCloud/StatsBand/DemoBand/ScaleSection/BigTestimonial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 import ScrollProgress from "@/components/motion/ScrollProgress";
-import AnnouncementBar from "@/components/layout/AnnouncementBar";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import Hero from "@/components/sections/Hero";
-import LogoCloud from "@/components/sections/LogoCloud";
-import StatsBand from "@/components/sections/StatsBand";
-import DemoBand from "@/components/sections/DemoBand";
+import ProblemsSection from "@/components/sections/ProblemsSection";
 import FeatureBlocks from "@/components/sections/FeatureBlocks";
 import GetStarted from "@/components/sections/GetStarted";
-import ScaleSection from "@/components/sections/ScaleSection";
-import BigTestimonial from "@/components/sections/BigTestimonial";
 import FAQ from "@/components/sections/FAQ";
 import CtaBand from "@/components/sections/CtaBand";
 
@@ -17,17 +12,12 @@ export default function Home() {
   return (
     <>
       <ScrollProgress />
-      <AnnouncementBar />
       <Navbar />
       <main>
         <Hero />
-        <LogoCloud />
-        <StatsBand />
-        <DemoBand />
+        <ProblemsSection />
         <FeatureBlocks />
         <GetStarted />
-        <ScaleSection />
-        <BigTestimonial />
         <FAQ />
         <CtaBand />
       </main>
